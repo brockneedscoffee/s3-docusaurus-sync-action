b7833679357e7d2a267f1ae29c9df591e9d2c686
Brock Davis <davisb@microsoft.com>
2021-09-16 03:33:40 -0400

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ echo "Run yarn build"
 yarn run build
 
 echo "Copying to website folder"
-aws s3 sync ./build/${PROJECT_NAME} s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
+aws s3 sync ./build s3://${AWS_S3_BUCKET} --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 
 echo "Cleaning up things"
 
